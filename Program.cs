b60883c7825b15993614e922591f7db548771015
Bahaa <bahaa.zewel@gmail.com>
2025-08-06 17:28:53 +0300

    added the opentelemetry for GRPC and updated the .ignore file

--- Program.cs
+++ Program.cs
@@ -25,6 +25,7 @@ builder.Services.AddOpenTelemetry()
 {
     tracing
         .AddAspNetCoreInstrumentation()
+        .AddGrpcClientInstrumentation()
         .AddHttpClientInstrumentation()
         .AddEntityFrameworkCoreInstrumentation();
     tracing.AddOtlpExporter(options =>
